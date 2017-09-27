d61c28d975a59e1e7a89cdb71cc87f868adbb0be
booleanoid <booleanoid@gmail.com>
2017-09-27 13:37:25 +0900

    add function

--- prime_number.py
+++ prime_number.py
@@ -8,10 +8,14 @@ def is_prime_number(num):
 
     return not(flg_non_prime_num)
 
-prime_numbers = []
+def get_prime_numbers(num):
+    prime_numbers = []
 
-for i in range(2, 200):
-    if is_prime_number(i) == True:
-        prime_numbers.append(i)
+    for i in range(2, num):
+        if is_prime_number(i) == True:
+            prime_numbers.append(i)
 
+    return prime_numbers
+
+prime_numbers = get_prime_numbers(200)
 print(prime_numbers)
