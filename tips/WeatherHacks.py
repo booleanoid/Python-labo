021a356f989bbf9602413e241b52b13ac3b27ba4
booleanoid <booleanoid@gmail.com>
2017-10-22 01:06:00 +0900

    add weather hacks

---
+++ tips/WeatherHacks.py
@@ -0,0 +1,7 @@
+import requests
+
+r = requests.get("http://weather.livedoor.com/forecast/webservice/json/v1?city=130010")
+print(r.status_code)
+print(r.text)
+json = r.json()
+print(json["description"]["text"])
