cb785a707c83f583988fa2967dc10f002e2d84fd
booleanoid <booleanoid@gmail.com>
2018-01-09 00:59:00 +0900

    mod tips_004

--- tips/tips004_list.py
+++ tips/tips004_list.py
@@ -2,8 +2,8 @@
 # coding: utf-8
 
 list_a = [1, 2, 3, 4, 5]
-list_b = list_a
-list_c = list_a.copy()
+list_b = list_a # shallow copy
+list_c = list_a.copy() # deep copy
 
 list_a[3] = 88
 print(list_b[3])
