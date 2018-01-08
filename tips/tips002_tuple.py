bb0f2e95b5dc2ce541c9c88889d11235fbb9b11f
booleanoid <booleanoid@gmail.com>
2018-01-09 00:30:56 +0900

    add tips

---
+++ tips/tips002_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+tup = (50, 88)
+
+# >>>tup[0]
+# 50
+# >>>tup[1]
+# 88
+
+# unpack the tuple
+
+sample_x, sample_y = tup
+
+# >>>sample_x
+# 50
+# >>>sample_y
+# 88
