c347280c346e87021bb9d6877b5970fc3d5c2219
booleanoid <booleanoid@gmail.com>
2018-01-09 00:47:30 +0900

    add tips

---
+++ tips/tips003_dictionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+red_hot_chili_Peppers = {
+    "vo" : "Anthony Kiedis",
+    "gt" : "Josh Klinghoffer",
+    "ba" : "Flea",
+    "dr" : "Chad Smith"
+}
+
+print(red_hot_chili_Peppers["ba"])
