2750d337757a72edbc1f97f2ca7481525a2df560
booleanoid <booleanoid@gmail.com>
2018-01-17 01:04:16 +0900

    set template

--- Django/mysite/myapp/views.py
+++ Django/mysite/myapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from.models import Add_Word
 
 # Create your views here.
 def index(request):
     data_list = Add_Word.objects.all()
-    return HttpResponse(data_list)
+    template = loader.get_template('myapp/index.html')
+    context = {
+        'lists': data_list
+    }
+    return HttpResponse(template.render(context, request))
