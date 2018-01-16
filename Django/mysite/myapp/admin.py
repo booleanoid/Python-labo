2a7dbaaa1fb20a45d7eaa880f4d0cd41d14ef1d2
booleanoid <booleanoid@gmail.com>
2018-01-17 00:54:39 +0900

    list display

--- Django/mysite/myapp/admin.py
+++ Django/mysite/myapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from.models import Add_Word
 
-# Register your models here.
-admin.site.register(Add_Word)
+class Add_WordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'word_text', 'date_time')
+
+admin.site.register(Add_Word, Add_WordAdmin)
