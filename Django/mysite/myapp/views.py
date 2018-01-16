053c9822405fe9c8d240d609d0c1b248df64cb05
booleanoid <booleanoid@gmail.com>
2018-01-17 01:15:31 +0900

    mod views

--- Django/mysite/myapp/views.py
+++ Django/mysite/myapp/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+#from django.http import HttpResponse
+#from django.template import loader
 from.models import Add_Word
 
-# Create your views here.
 def index(request):
     data_list = Add_Word.objects.all()
-    template = loader.get_template('myapp/index.html')
+    #template = loader.get_template('myapp/index.html')
     context = {
         'lists': data_list
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'myapp/index.html', context)
