327c7017c7e84ac1ab6a1a4fda8cd52d8dbf2b09
booleanoid <booleanoid@gmail.com>
2018-01-19 03:00:50 +0900

    Change name date_time

--- Django/mysite/myapp/models.py
+++ Django/mysite/myapp/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Add_Word(models.Model):
     word_text = models.CharField(max_length = 140)
-    date_time = models.DateTimeField('保存日')
+    date_time = models.DateTimeField('saved date')
