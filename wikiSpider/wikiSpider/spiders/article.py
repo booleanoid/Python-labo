1a93b0082929a8fb9cc19c0d2935f520bcd5260b
booleanoid <booleanoid@gmail.com>
2018-09-02 19:49:25 +0900

    get page title data

--- wikiSpider/wikiSpider/spiders/article.py
+++ wikiSpider/wikiSpider/spiders/article.py
@@ -2,20 +2,26 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-
+from wikiSpider.items import Article
 
 class ArticleSpider(CrawlSpider):
     name = 'article'
     allowed_domains = ['en.wikipedia.org']
-    start_urls = ['http://en.wikipedia.org/']
+    start_urls = ['http://en.wikipedia.org/wiki/Main-Page']
 
     rules = (
         Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
 
-    def parse_item(self, response):
-        i = {}
+    def parse(self, response):
+        # i = {}
         #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
         #i['name'] = response.xpath('//div[@id="name"]').extract()
         #i['description'] = response.xpath('//div[@id="description"]').extract()
-        return i
+        # return i
+        item = Article()
+        title = response.xpath('//h1/text()')[0].extract()
+        print('Title is ' + title)
+        item['title'] = title
+        return item
+
