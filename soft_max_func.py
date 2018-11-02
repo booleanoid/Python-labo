219bea7315a70921f53b6f8317f5ba29530e06f3
booleanoid <booleanoid@gmail.com>
2018-11-02 22:16:58 +0900

    add softmax func

---
+++ soft_max_func.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# exp_a = np.exp(a)
+#
+# sum_exp_a = np.sum(exp_a)
+# y = exp_a / sum_exp_a
+# print(y)
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    return exp_a / sum_exp_a
+
+a = np.array([0.3, 2.9, 4.0])
+y =  softmax(a)
+print(y)
+
+
