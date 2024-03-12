7b6f253217d9d475322837d53d5416ce1020a222
booleanoid <booleanoid@gmail.com>
2024-03-13 01:03:19 +0900

    add scripts

---
+++ hydra/main.py
@@ -0,0 +1,12 @@
+﻿import hydra
+from omegaconf import DictConfig
+
+
+@hydra.main(config_name="config", version_base=None, config_path="config")
+def main(cfg: DictConfig) -> None:
+    print(cfg.model.dropout_rate)
+    print(cfg.model.num_heads)
+
+
+if __name__ == "__main__":
+    main()
