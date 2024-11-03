6a5a3edc188a9a14f5109222da5a776c9acb97e5
booleanoid <booleanoid@gmail.com>
2024-11-04 01:47:57 +0900

    移動平均を修正

--- stock_data/main.py
+++ stock_data/main.py
@@ -12,6 +12,19 @@ print(df)
 
 # 東電9501
 df_9501 = get_stock_data(9501)
-cdf = df_9501.tail(100)
-mpf.plot(cdf, type="candle", figratio=(16,9), volume=True, mav=(5, 25, 75), style="nightclouds")
+
+df_9501["ma5"] = df_9501["Close"].rolling(window=5).mean()
+df_9501["ma25"] = df_9501["Close"].rolling(window=25).mean()
+df_9501["ma75"] = df_9501["Close"].rolling(window=75).mean()
+
+
+cdf = df_9501[dt.datetime(2023, 1, 1):dt.datetime(2023, 12, 31)]
+apd = [
+  mpf.make_addplot(cdf["ma5"], color="blue"),
+  mpf.make_addplot(cdf["ma25"], color="green"),
+  mpf.make_addplot(cdf["ma75"], color="red")
+  ]
+
+mpf.plot(cdf, type="candle", figratio=(16,9), addplot=apd)
+# mpf.plot(cdf, type="candle", figratio=(16,9), volume=True, mav=(5, 25, 75), style="nightclouds")
 
