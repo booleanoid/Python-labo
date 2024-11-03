388b68e5614a2c0187104c1516fc7758521bd2ba
booleanoid <booleanoid@gmail.com>
2024-11-04 01:25:47 +0900

    グラフサンプル

--- stock_data/main.py
+++ stock_data/main.py
@@ -9,3 +9,8 @@ def get_stock_data(code):
 
 df = pdr.DataReader("9434.JP", "stooq")
 print(df)
+
+# 東電9501
+df_9501 = get_stock_data(9501)
+cdf = df_9501.tail(100)
+mpf.plot(cdf, type="candle", figratio=(16,9), volume=True, style="nightclouds")
